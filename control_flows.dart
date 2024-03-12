c36a2ba9d4989e2469b660655483e49f8cb8f302
Fredy2024 <fredobonyo5@gmail.com>
2024-03-12 20:25:57 +0300

    control flow in dart, conditional statement

---
+++ control_flows.dart
@@ -0,0 +1,14 @@
+void main() {
+//if statement output depends on the conditional expression.
+ var num = 10;
+ if (num > 10) {
+ print("Your number is greater than 10");
+ }
+ else if (num < 10) {
+ print("Your number is less than 10");
+ }
+//if else if statement
+if (num==10) {
+  print ("Your number is equal to 10");
+ }
+}
